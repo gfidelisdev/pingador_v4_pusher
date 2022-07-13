22f28354f8e48c38b420a04dc82387cf21f65a8c
gfidelisdev <109247627+gfidelisdev@users.noreply.github.com>
2022-07-13 19:02:40 -0300

    socket communication

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 var app = require('express')()
 var env = require('./.env')
-
+var cors = require('cors')
+app.use(cors())
 var http = require('http').createServer(app)
 var io = require('socket.io')(http)
-
 http.listen(env.PORT,()=>{
     console.log(`listening on PORT ${env.PORT}`)
 })
 
-app.get('/', (req, res)=>{
-    res.send('ok')
-})
+// app.get('/', (req, response)=>{
+//     response.setHeader('Access-Control-Allow-Origin', req.header.origin);
+// response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+// response.setHeader('Access-Control-Max-Age', 2592000); // 
+//     response.send('ok')
+// })
 
 io.on('connection', (socket)=>{
-    console.log(`new client connected, ${socket}`)
+    console.log(`new client connected`)
+    socket.emit('connection', null)
+    socket.on('nuevo', msg=>{
+        console.log(msg)
+    })
 })
